2644d2e9d18a0937da66cc543dcc6bc5c144d5d0
kimutomo <k17rs042@st.kyusan-u.ac.jp>
2020-06-01 09:34:24 +0900

    Create array.rb
    配列の練習

---
+++ array.rb
@@ -0,0 +1,16 @@
+ia = [10,20,30,40]
+
+i = 0
+while i < ia.length
+    print ia[i].to_s + ","
+    i += 1
+end
+puts
+
+sa = ["January","February","March"]
+i = 0
+while i < sa.length
+    print sa[i] + ","
+    i += 1
+end
+puts
