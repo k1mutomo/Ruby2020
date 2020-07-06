41ed67a32fee003ad1b3554d681dc5e2c181787e
kimutomo <k17rs042@st.kyusan-u.ac.jp>
2020-07-06 10:18:02 +0900

    Create httpclient11.rb

---
+++ httpclient11.rb
@@ -0,0 +1,16 @@
+require "socket"
+
+port=80
+host = ARGV[0]
+path = ARGV[1]
+
+TCPSocket.open host, port do |sock|
+    sock.puts "GET #{path} HTTP/1.1"
+    sock.puts "HOST: #{host}"
+    sock.puts "Connection: close"
+    sock.puts ""
+
+    while line=sock.gets
+        puts line
+    end
+end
