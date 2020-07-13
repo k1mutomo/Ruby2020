025a73ded79be36c9ea25a9156da50f34b8ef0ab
kimutomo <k17rs042@st.kyusan-u.ac.jp>
2020-07-13 10:27:14 +0900

    Update httpserver1.rb

--- httpserver1.rb
+++ httpserver1.rb
@@ -1,15 +1,21 @@
 require 'socket'
 
-s = TCPServer.open 80
-
-while true
-    sock = s.accept
+def server sock
     while line = sock.gets
+        sleep 2
         line.chomp!
         break if line == ""
         p line
         sock.puts "receive: #{line}"
     end
     sock.close
+end
+    
+s = TCPServer.open 80
+
+while true
+    Thread new s.accept do |sock|
+        server sock
+    end
 end
 s.close
